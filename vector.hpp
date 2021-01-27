d3fceadddb32245a57141bace5a8d9ecd159d731
lspiess <lambert.spiess@gmail.com>
2021-01-27 01:54:12 +0100

    adsdfs

--- vector.hpp
+++ vector.hpp
@@ -136,7 +136,18 @@ namespace ft
 				while (ptr != _end) { _alloc.destroy(ptr++); }
 			}
 
-			
+			reference operator[] (size_type n) { return(_arr[n]); }
+			const_reference operator[] (size_type n) const { return(_arr[n]); }
+			reference at(size_type n)
+			{
+				if (n >= size()) { throw std::out_of_range("out of range"); }
+				return (_start[n]);
+			}
+			reference at(size_type n) const
+			{
+				if (n >= size()) { throw std::out_of_range("out of range"); }
+				return (_start[n]);
+			}
 
 	}; // class vector
 
